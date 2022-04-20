3227c814df9009dc76641075e5adcf1701c1bc5b
dqawami <daryan.qawami@gmail.com>
2022-04-20 00:23:05 -0700

    Added working weight sending

--- wireless_trainer.py
+++ wireless_trainer.py
@@ -1,13 +1,14 @@
+import math
 import socket
+import torch
 
 class WirelessTrainer:
     def __init__(self, inet_type, ip_address: str, sock: int, buffer: int, 
-                 epochs: int, batch_size: int, criterion, wait=5):
+                 epochs: int, batch_size: int, wait=5):
         assert inet_type == 'client' or inet_type == 'server'
 
         self.buffer = buffer
         self.batch_size = batch_size
-        self._criterion = criterion
 
         self.net_node = socket.socket()
         net_details = (ip_address, sock)
@@ -28,9 +29,35 @@ class WirelessTrainer:
         self.total_batch = batch_size + self.other_batch
 
     def send(self, data):
+        '''sendable_data = str(data)
+
+        num_packets = int(math.ceil(len(sendable_data) / self.buffer))
+        print(str(num_packets).encode())
+        print(str(num_packets).encode().decode())
+
+        self.net_node.send((str(num_packets) + ' ').encode())
+
+        for i in range(num_packets):
+            end = min(self.buffer * (i + 1), len(sendable_data))
+            temp_data = sendable_data[(self.buffer * i):end]
+            self.net_node.send(temp_data.encode())'''
         self.net_node.send(str(data).encode())
 
     def recv(self, cast_type):
+        '''received_data = self.net_node.recv(self.buffer).decode()
+        num_packets = received_data.split()[0]
+        received_data = received_data.split(' ', 1)[1]
+        print(num_packets)
+        num_packets = int(num_packets)
+        print(num_packets)
+
+        if received_data != '':
+            num_packets -= 1
+
+        for _ in range(num_packets):
+            received_data += self.net_node.recv(self.buffer).decode()
+
+        return cast_type(received_data)'''
         return cast_type(self.net_node.recv(self.buffer).decode())
 
     def send_and_recv(self, data):
@@ -42,11 +69,20 @@ class WirelessTrainer:
         trainset, _ = splitter(trainset, [subset, len(trainset) - subset])
         return trainset
 
-    def criterion(self, outputs, labels):
-        loss = self._criterion(outputs, labels)
-        other_loss = self.send_and_recv(loss.item())
+    def recursive_grad_averaging(self, layer):
+        out = []
+        buffer = []
+        for l in layer:
+            if type(l) == list:
+                out.append(self.recursive_grad_averaging(l))
+            else:
+                temp = str(l) + ' '
+                other = self.send_and_recv(temp)
+                buffer.extend(other.split(' ')[:-1])
+                out.append((l * self.batch_size + float(buffer.pop(0)) * self.other_batch)
+                            / self.total_batch)
+        return out
 
-        total_loss = (loss * self.batch_size + 
-                      other_loss * self.other_batch) / self.total_batch
-
-        return total_loss
+    def grad_averging(self, model):
+        for p in model.parameters():
+            p.grad = torch.tensor(self.recursive_grad_averaging(p.grad.tolist()))
